b867b52f65a445178d6f7e200186bfad25b850de
hinst <hinstance@yandex.ru>
2017-12-25 00:58:29 +0300

    path

---
+++ src/WebPath.ts
@@ -0,0 +1,7 @@
+namespace hts {
+    export class WebPath {
+        public debugMode = true;
+        public constructor() {
+        }
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 namespace hblog {
 
-    export var webPath: String;
+    export var webPath: string;
+    export var pagePath: string = document.location.href;
 
     export class App {
         public static main(): number {
             console.log('Now running; web path is: "' + webPath + '"; page path is: "' + document.location.pathname + '"');
+            const app = new App();
+            app.run();
             return 0;
+        }
+
+        public run(): void {
+            console.log(App.checkRoute("/h-blog"));
+        }
+
+        public static checkRoute(path: string): boolean {
+            return document.location.pathname.indexOf(path) == 0;
         }
     }
 
