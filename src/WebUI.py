e1f0cfdd54df943c331f872010607d2e66bce6bb
hinst <hinstance@yandex.ru>
2017-12-24 21:24:42 +0300

    set fileDeliverer name to avoid "view added twice" error from Flask

--- src/WebUI.py
+++ src/WebUI.py
@@ -17,16 +17,21 @@ class WebUI:
         def js_bin(fileName):
             return send_file('../js-bin/' + fileName, conditional=True)
 
+        self.registerStaticFileFolder('html-bin')
+        self.registerStaticFileFolder('css-3rd')
+
     def registerStaticFileFolder(self, folder: str):
-        @self.flask.route(self.webPath + '/' + folder + '/<string:fileName>')
+        url = self.webPath + '/' + folder + '/<string:fileName>'
         def fileDeliverer(fileName):
             return send_file('../' + folder + '/' + fileName, conditional=True)
+        fileDeliverer.__name__ = 'fileDeliverer_' + folder
+        self.flask.add_url_rule(url, view_func=fileDeliverer)
     
     def prepareLayout(self):
         with open('src/layout.html', 'r') as file:
             content: str = file.read()
             content = content.replace('_web_path_', self.webPath)
-            with open('html-bin/layout.html', 'r') as outputFile:
+            with open('html-bin/layout.html', 'w') as outputFile:
                 outputFile.write(content)
 
     def debugListFiles(self):
