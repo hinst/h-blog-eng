32317c7b6494a33852548d5fa5cbbdd252df18b3
hinst <hinstance@yandex.ru>
2017-12-24 23:13:21 +0300

    register web file dirs

--- src/WebUI.py
+++ src/WebUI.py
@@ -3,19 +3,18 @@ from os import listdir
 from os.path import isfile, join
 
 class WebUI:
+    fileDirs = ['css-3rd', 'js-3rd', 'html-bin', 'js-bin']
+
     def __init__(self, flask: Flask = None, webPath = "/h-blog"):
         self.flask = flask
         self.webPath = webPath
         if False: self.debugListFiles()
         self.prepareLayout()
-
         @flask.route(webPath)
         def main():
             return send_file("../html-bin/layout.html", conditional=True)
-
-        self.registerStaticFileFolder('html-bin')
-        self.registerStaticFileFolder('js-bin')
-        self.registerStaticFileFolder('css-3rd')
+        for fileDir in self.fileDirs:
+            self.registerStaticFileFolder(fileDir)
 
     def registerStaticFileFolder(self, folder: str):
         url = self.webPath + '/' + folder + '/<string:fileName>'
