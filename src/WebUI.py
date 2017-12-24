219c836a73a1fd923e1bde9b82d1e4e76f420d0a
hinst <hinstance@yandex.ru>
2017-12-24 21:10:14 +0300

    constructing layout

--- src/WebUI.py
+++ src/WebUI.py
@@ -5,6 +5,9 @@ from os.path import isfile, join
 class WebUI:
     def __init__(self, flask: Flask = None, webPath = "/h-blog"):
         self.flask = flask
+        self.webPath = webPath
+        if False: self.debugListFiles()
+        self.prepareLayout()
 
         @flask.route(webPath)
         def main():
@@ -13,5 +16,22 @@ class WebUI:
         @flask.route(webPath + '/js-bin/<string:fileName>')
         def js_bin(fileName):
             return send_file('../js-bin/' + fileName, conditional=True)
+
+    def registerStaticFileFolder(self, folder: str):
+        @self.flask.route(self.webPath + '/' + folder + '/<string:fileName>')
+        def fileDeliverer(fileName):
+            return send_file('../' + folder + '/' + fileName, conditional=True)
+    
+    def prepareLayout(self):
+        with open('src/layout.html', 'r') as file:
+            content: str = file.read()
+            content = content.replace('_web_path_', self.webPath)
+            print(content)
+
+    def debugListFiles(self):
+        print("Files:")
+        for file in listdir("."):
+            print(file)
+
 
 
