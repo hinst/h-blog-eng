eb08f967b8b09ed9ee4c1bcadbf9653f42c1206e
hinst <hinstance@yandex.ru>
2017-12-25 23:44:43 +0300

    log file name format changed

--- orangeStart.sh
+++ orangeStart.sh
@@ -1,2 +1,2 @@
 cd /home/hinst/ufd/App/h-blog-eng
-/usr/local/bin/python3.6 src/program.py >>"log/$(date +"%d-%m-%Y").log" 2>&1
+/usr/local/bin/python3.6 src/program.py >>"log/$(date +"%Y-%m-%d").log" 2>&1
