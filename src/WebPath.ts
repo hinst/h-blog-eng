7347ce13e629f01e80f678cb7cc6d2af742cecd2
hinst <hinstance@yandex.ru>
2017-12-26 00:06:33 +0300

    bugs fixed in WebPath.ts

--- src/WebPath.ts
+++ src/WebPath.ts
@@ -19,13 +19,14 @@ namespace hts {
                 query = query.substring(1);
             if (query.length > 0) {
             const rows = query.split("&");
-                for (const row in rows) {
+                for (const row of rows) {
                     const parts = row.split("=");
                     if (parts.length < 2)
                         parts.push("");
-                    this.args[parts[0]] = parts[1];
+                    args[parts[0]] = parts[1];
                 }
             }
+            this.args = args;
         }
 
         public checkRouteMatch(route: string) {
