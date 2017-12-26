541c0edf95fb742d1ee3f4572d46500df22f7c74
Alexandr Savinykh <asavinykh@mfisoft.local>
2017-12-26 13:55:49 +0300

    Removed pathparser.js because I decided not to use it. Added new blog post: UI components and TypeScript. Added highlight.pack.js. Code highlighting now works.

--- src/EntryPanel.ts
+++ src/EntryPanel.ts
@@ -29,19 +29,27 @@ namespace hblog {
             header.css("margin-top", "8px");
             header.css("margin-bottom", "8px");
             return header;
-        }
+        }   
 
         makeBody(data: string) {
             const content = this.transformText(data);
             const body = Panel.createElement("div");
             body.css("max-width", "1000px");
             body.html(content);
+            const codes = body.children("pre");
+            codes.css("font-family", "Consolas, Monospace");
+            codes.css("font-size", "12px");
+            codes.each((i, code) => {
+                $(code).children("br:first").remove();
+                code.innerHTML = code.innerHTML.trim();
+                window["hljs"].highlightBlock(code);
+            });
             return body;
         }
 
         transformText(text: string): string {
             text = hts.WebPath.escapeHtml(text).replace(/\n/g, "<br/>");
-            text = text.replace(/#hcode-begin/g, "<code>").replace(/#hcode-end/g, "</code>");
+            text = text.replace(/#hcode-begin/g, "<pre>").replace(/#hcode-end/g, "</pre>");
             return text;
         }
 
