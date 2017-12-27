3540e8f4b422fe2a68d18b5dee4c237030a33477
Alexandr Savinykh <asavinykh@mfisoft.local>
2017-12-27 13:05:48 +0300

    EntryContentTransformer

--- src/EntryContentTransformer.ts
+++ src/EntryContentTransformer.ts
@@ -2,9 +2,28 @@ namespace hblog {
     export class EntryContentTransformer {
         constructor(source: string) {
             const lines = source.split("\n");
+            let inCode = false;
             for (let i = 0; i < lines.length; i++) {
-                lines[0] = lines[0].replace(/\r/g, "");
+                lines[i] = lines[i].replace(/\r/g, "");
+                const trimmedLine = lines[i];
+                if (trimmedLine == "#hcode-typescript-begin") {
+                    lines[i] = '<pre class="typescript">';
+                    inCode = true;
+                }
+                if (trimmedLine == "#hcode-begin") {
+                    lines[i] = '<pre>';
+                    inCode = true;
+                }
+                if (!inCode)
+                    lines[i] = lines[i] + "<br>";
+                if (trimmedLine == "#hcode-end") {
+                    lines[i] = '</pre>';
+                    inCode = false;
+                }
             }
+            if (inCode)
+                lines.push('</pre>');
+            this.result = lines.join("");
         }
 
         result: string;
