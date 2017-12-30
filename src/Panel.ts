5dc31a20b465ed6cf0f59a3089b357716a2c0813
hinst <hinstance@yandex.ru>
2017-12-30 23:08:41 +0300

    createCenterOverlay

--- src/Panel.ts
+++ src/Panel.ts
@@ -16,5 +16,18 @@ namespace hblog {
 
         public refresh() {
         }
+
+        public createCenterOverlay(child: JQuery) {
+            const outer = Panel.createElement("div");
+            outer.css("display", "table");
+            outer.css("width", "100%");
+            outer.css("height", "100%");
+            outer.css("position", "absolute");
+            const middle = Panel.createElement("div");
+            middle.css("display", "table-cell");
+            middle.css("vertical-align", "middle");
+            middle.append(child);
+            outer.append(middle);
+        }
     }
 }
