7039485cf51c014e8a26c556c5c867cd3e7197c1
hinst <hinstance@yandex.ru>
2017-12-30 15:51:02 +0300

    Wrote addComment function

--- src/DB.py
+++ src/DB.py
@@ -1,10 +1,24 @@
 import sqlite3
 from GoogleUser import *
+from datetime import datetime
 
 class DbUserRow:
     def __init__(self):
+        self.rowid = 0
+        self.userId = ""
         self.name = ""
-        self.id = ""
+
+    def asDbDict(self):
+        return {'userId': self.userId, 'name': self.name}
+
+class DbCommentRow:
+    def __init__(self):
+        self.userId = ""
+        self.moment = str(datetime.now())
+        self.content = ""
+
+    def asDbDict(self):
+        return {'userId': self.userId, 'moment': self.moment, 'content': self.content}
 
 class DB:
     def __init__(self):
@@ -16,15 +30,16 @@ class DB:
         connection.execute("CREATE TABLE IF NOT EXISTS users(userId TEXT PRIMARY KEY, name TEXT)")
         connection.close()
 
+    def connect(self):
+        return sqlite3.connect(self.dbFilePath)
+
     def updateUser(self, connection: sqlite3.Connection, user: DbUserRow):
         existingUser = self.readUserById(connection, user.id)
         if existingUser != None:
             if existingUser.name != user.name:
-                connection.execute("update users set name=:name where userId=:userId", 
-                    {'name': user.name, 'userId': user.id})
+                connection.execute("update users set name=:name where userId=:userId", user.asDbDict())
         else:
-            connection.execute("insert into users (userId, name) values (:userId, :name)",
-                {'name': user.name, 'userId': user.id})
+            connection.execute("insert into users (userId, name) values (:userId, :name)", user.asDbDict())
 
     def readUserById(self, connection: sqlite3.Connection, id: str) -> DbUserRow:
         result: DbUserRow = None
@@ -36,9 +51,17 @@ class DB:
             print("users")
             print(users)
             result = DbUserRow()
-            result.id = user[0]
+            result.userId = user[0]
             result.name = user[1]
         return result
     
-    def connect(self):
-        return sqlite3.connect(self.dbFilePath)
+    def addCommentDirect(self, connection: sqlite3.Connection, commentRow: DbCommentRow):
+        cursor = connection.cursor()
+        cursor.execute("insert into comments (userId, moment, content) values (:userId, :moment, :content)", commentRow.asDbDict())
+
+    def addComment(self, connection: sqlite3.Connection, user: DbUserRow, content: str):
+        self.updateUser(connection, user)
+        comment = DbCommentRow()
+        comment.userId = user.userId
+        comment.content = content
+        self.addCommentDirect(connection, comment)
